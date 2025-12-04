65595c92261fedd3c01ac04e063b6ebfddf0fa5b
Даниелян Даниел Рафаелович <danielian.dr@phystech.edu>
2025-12-04 15:26:29 +0300

    fix

--- dbtest.py
+++ dbtest.py
@@ -10,7 +10,7 @@ from sqlalchemy.sql import func
 from datetime import datetime
 from typing import Optional, List
 
-DATABASE_URL = "sqlite:///database.db"
+DATABASE_URL = "postgresql+psycopg2://@localhost:5432/testdb"
 
 engine = create_engine(DATABASE_URL, echo=True)
 Base = declarative_base()
